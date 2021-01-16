c131b41ef56af5fb0fc12df3276309ca0ae00dca
Faraz Akhtar <faraz@Farazs-Air.fios-router.home>
2021-01-16 15:25:05 -0500

    test

--- client/Components/App.js
+++ client/Components/App.js
@@ -2,17 +2,42 @@ import React, { Component } from 'react';
 
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-//
+// Swtich: it searches through its children <Route> elements to find one whose path matches the current URL
+// One important thing to note is that a <Route path> matches the beginning of the URL, not the whole thing. So a <Route path="/"> will always match the URL. Because of this, we typically put this <Route> last in our <Switch>. Another possible solution is to use <Route exact path="/"> which does match the entire URL.
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: 'test',
+      navigation: [
+        {
+          _id: 1,
+          name: 'Note Book 1',
+        },
+        {
+          _id: 2,
+          name: 'Note Book 2',
+        },
+        {
+          _id: 3,
+          name: 'Note Book 3',
+        },
+      ],
+      notebookDetails: null,
     };
   }
 
   render() {
+    const generateLinks = this.state.navigation.map((notebook) => {
+      const { _id: id, _id: reactKey, name } = notebook;
+
+      return (
+        <li key={reactKey}>
+          <Link to="/{name}}">{name}</Link>
+        </li>
+      );
+    });
+
     return (
       <div>
         <nav>
@@ -20,12 +45,7 @@ class App extends Component {
             <li>
               <Link to="/">Home Page!</Link>
             </li>
-            <li>
-              <Link to="/notebook1">Note Book 1</Link>
-            </li>
-            <li>
-              <Link to="/notebook2">Note Book 2</Link>
-            </li>
+            {generateLinks}
           </ul>
         </nav>
         <Switch>
