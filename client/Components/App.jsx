cd6fb412261a4ed2fbf48f45db098f8911151e01
ed-deng <edeng2@binghamton.edu>
2021-01-19 00:33:20 -0500

    d

--- client/Components/App.jsx
+++ client/Components/App.jsx
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import Notebook from './Notebook.jsx';
-import Home from './Home.jsx';
+import Notebook from "./Notebook.jsx";
+import Home from "./Home.jsx";
 
 // Swtich: it searches through its children <Route> elements to find one whose path matches the current URL
 // One important thing to note is that a <Route path> matches the beginning of the URL, not the whole thing. So a <Route path="/"> will always match the URL. Because of this, we typically put this <Route> last in our <Switch>. Another possible solution is to use <Route exact path="/"> which does match the entire URL.
@@ -24,14 +24,18 @@ class App extends Component {
 
   componentDidMount() {
     //fetches all information on all notebooks
-    fetch('/api/')
+    fetch("/api/")
       .then((res) => res.json())
       .then((data) => this.setState({ notebookList: data }));
   }
 
   render() {
     const generateLinks = this.state.notebookList.map((notebook) => {
-      const { _id: id, _id: reactKey, name } = notebook;
+      const {
+        notebook_id: id,
+        notebook_id: reactKey,
+        notebook_name: name,
+      } = notebook;
       console.log(id, name);
       return (
         <li key={reactKey}>
@@ -42,9 +46,9 @@ class App extends Component {
 
     const generateNotebookRoutes = this.state.notebookList.map((notebook) => {
       const {
-        _id: id,
-        _id: reactKey,
-        name,
+        notebook_id: id,
+        notebook_id: reactKey,
+        notebook_name: name,
         description,
         skills,
         reminders,
